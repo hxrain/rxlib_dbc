e1721d354a7912a5efe4b8f03c65d9914d05fabf
Rain <hxrain@users.noreply.github.com>
2018-12-05 17:53:32 +0800

    增加mysql功能封装的测试用例工程.

---
+++ test/ut_mysql/ut_dbc.cpp
@@ -0,0 +1,28 @@
+
+#define RX_DEF_ALLOC_USE_STD 1          //是否使用标准malloc分配器进行测试(可用于检测内存泄露)
+
+#include "../ut_dbc_base.h"
+
+#include "stdlib.h"
+#include "rx_tdd.h"
+
+#if RX_CC==RX_CC_VC
+    #include <crtdbg.h>
+#endif
+
+int main()
+{
+#if RX_CC==RX_CC_VC
+    //_CrtSetBreakAlloc(74);
+#endif
+
+    rx_tdd_run();
+
+    getchar();
+
+#if RX_CC==RX_CC_VC
+    _CrtDumpMemoryLeaks();
+#endif
+    return 0;
+}
+
